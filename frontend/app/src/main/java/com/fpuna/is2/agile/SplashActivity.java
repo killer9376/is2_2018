a203283c5bf1910bc65f3d2c8320a3add2a8d671
killer9376 <36566208+killer9376@users.noreply.github.com>
2018-06-24 11:42:29 -0400

    Nav_
    Se agregó nav, falta login antes del nav

--- frontend/app/src/main/java/com/fpuna/is2/agile/SplashActivity.java
+++ frontend/app/src/main/java/com/fpuna/is2/agile/SplashActivity.java
@@ -4,17 +4,24 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
     public final int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this, NavActivity.class);
                 startActivity(intent);
                 finish();
             }
